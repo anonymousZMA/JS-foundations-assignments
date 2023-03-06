18c19d282abe2bfcbdbb6da45e299609b109c43e
anonymousZMA <alwarda@hotmail.co.uk>
2023-03-06 11:55:15 +0300

    Create a script file
    Create a script file that contains all the logic behind the game.
    This file contains three main functions, computerPlay is responsible for randomly selects from the selections list, playRound function which takes in two parameters on for payerSelection and the other for computerSelections and this function is responsible for displaying the result of the game, and the last function which is game and this one is responsible for limiting the game for 5 attempts only.

---
+++ Main-JS-Project/script.js
@@ -0,0 +1,65 @@
+const selections = ["Rock", "Paper", "Scissors"];
+let computerScore = 0;
+let playerScore = 0;
+
+function computerPlay() {
+  return selections[Math.floor(Math.random() * selections.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == ("" || null))
+    return `Hey there! Don't be shy, make your move! But remember, leaving the input field empty won't do you any good. So go ahead and show that computer who's boss!`;
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  if (playerSelection !== ("rock" || "paper" || "scissors"))
+    return `Well, somebody's clearly been hitting the wrong buttons. Keep calm and try again, friend!`;
+  if (computerSelection == "rock" && playerSelection == "scissors") {
+    computerScore++;
+    return `You Lost!, ${computerSelection} beats ${playerSelection}`;
+  } else if (computerSelection == "scissors" && playerSelection == "paper") {
+    computerScore++;
+    return `You Lost!, ${computerSelection} beats ${playerSelection}`;
+  } else if (computerSelection == "paper" && playerSelection == "rock") {
+    computerScore++;
+    return `You Lost!, ${computerSelection} beats ${playerSelection}`;
+  } else if (computerSelection == playerSelection) {
+    return `Well, it's a tie`;
+  } else {
+    playerScore++;
+    return `Congrats! You won! ${playerSelection} beats ${computerSelection}`;
+  }
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Please enter you selection ");
+    console.log("🚀 ~ file: script.js:24 ~ playerSelection:", playerSelection);
+
+    const computerSelection = computerPlay();
+    console.log(
+      "🚀 ~ file: script.js:26 ~ computerSelection:",
+      computerSelection
+    );
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(
+      `Computer Score: ${computerScore} - Player Score: ${playerScore}`
+    );
+    if (i !== 4) {
+      console.log(
+        `No pressure, but you only have ${4 - i} more shots at greatness!`
+      );
+    } else {
+      console.log(
+        `Sorry, game over! You've used up all your attempts. Better luck next time!`
+      );
+    }
+    console.log("-".repeat(100));
+  }
+  if (computerScore > playerScore) {
+    console.log(`Niahaha 😈 Machine Beat Human`);
+  } else {
+    console.log(`Well, Congrats! you won 🥴`);
+  }
+}
+
+game();
